ffd9591ca77d6a2550df0b0f52345a52a75a8ab0
Code Sandbox <hello@codesandbox.io>
2021-08-11 19:33:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.91f10ab0c815d534a9946f3f7c1573fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52e63b926f170575717527c405088a5",
+    "url": "https://dbomcconville.github.io/csb-952vq/index.html"
+  },
+  {
+    "revision": "f27c5f947116f71fd186",
+    "url": "https://dbomcconville.github.io/csb-952vq/static/js/2.1a7a7da3.chunk.js"
+  },
+  {
+    "revision": "e15a82fae1ff8b30527f",
+    "url": "https://dbomcconville.github.io/csb-952vq/static/js/main.1c9ba716.chunk.js"
+  },
+  {
+    "revision": "ea84ac1663f16b14594c",
+    "url": "https://dbomcconville.github.io/csb-952vq/static/js/runtime-main.aaeada36.js"
+  }
+]);
